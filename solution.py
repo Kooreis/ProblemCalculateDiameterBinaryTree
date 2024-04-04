f43753e41d386c0523f2fefed652b224b239dc98
Kooreis <kooreis.official@gmail.com>
2024-04-04 14:14:53 -0500

    This update adds the height function. This function calculates the height of a binary tree by recursively calling itself for the left and right child of a node and returning the maximum height plus one.

--- solution.py
+++ solution.py
@@ -1,5 +1,4 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+def height(node):
+    if node is None:
+        return 0
+    return 1 + max(height(node.left), height(node.right))
